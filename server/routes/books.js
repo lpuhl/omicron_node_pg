28d3ba7f9090b4766a24abbfb66900bae05964ab
Liz Puhl <eapuhl@gmail.com>
2016-08-18 14:13:28 -0500

    genre selector added

--- server/routes/books.js
+++ server/routes/books.js
@@ -8,16 +8,42 @@ router.get('/', function (req, res) {
   // res.send({message: 'OK'});
   pg.connect(connectionString, function (err, client, done) {
     if (err) {
+      console.log('cannot connect');
       res.sendStatus(500);
     }
     client.query('SELECT * FROM books', function (err, result) {  //"result" param can be called anything
     done(); // Done with this connection. Can only have 10 queries running at a time.
       if (err) {
+        console.log('bad query');
         res.sendStatus(500);
       }
       res.send(result.rows);
     });  // first parameter is SQL query; second is a function
   });
+});
+
+router.get('/:genre', function (req, res) {
+  var genre = req.params.genre;
+  console.log(req.params.genre);
+
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      res.sendStatus(500);
+    }
+    client.query('SELECT * FROM books ' +
+                  'WHERE genre = $1',
+                  [genre],
+                  function (err, result) {
+                    done();
+                    if (err) {
+                      console.log(err);
+                      res.sendStatus(500);
+                    } else {
+                    res.send(result.rows);
+                    }
+                });
+  });
+
 });
 
 router.post('/', function (req, res) {
@@ -27,9 +53,9 @@ router.post('/', function (req, res) {
     if (err) {
       res.sendStatus(500);
     }
-    client.query('INSERT INTO books (author, title, published, edition, publisher)'
-                + 'VALUES ($1, $2, $3, $4, $5)', // "prepared statement"
-              [book.author, book.title, book.published, book.edition, book.publisher],
+    client.query('INSERT INTO books (author, title, published, genre)'
+                + 'VALUES ($1, $2, $3, $4)', // "prepared statement"
+              [book.author, book.title, book.published, book.genre],
               function (err, result) {
                 done();
 
@@ -39,6 +65,56 @@ router.post('/', function (req, res) {
                 res.sendStatus(201);
               });
   });
+});
+
+router.put('/:id', function (req, res) {
+  var id = req.params.id;
+  var book = req.body;
+
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      res.sendStatus(500);
+    }
+    client.query('UPDATE books ' +
+                  'SET title = $1, ' +
+                  'author = $2, ' +
+                  'published = $3, ' +
+                  'genre = $4 ' +
+                  'WHERE id = $5',
+                [book.title, book.author, book.published, book.genre, id],
+              function (err, result) {
+                done();
+                if (err) {
+                  console.log('err', err);
+                  res.sendStatus(500);
+                } else {
+                  res.sendStatus(200);
+                }
+              });
+  });
+})   //:id can be named anything. Colon is the impt part.
+
+router.delete('/:id', function (req, res) {
+  var id = req.params.id;
+
+  pg.connect(connectionString, function (err, client, done) {
+    if (err) {
+      res.sendStatus(500);
+    }
+    client.query('DELETE FROM books ' +
+                  'WHERE id = $1',
+                  [id],
+                  function (err, result) {
+                    done();
+
+                    if (err) {
+                      res.sendStatus(500);
+                    } else {
+                    res.sendStatus(200);
+                  }
+                });
+  });
+
 });
 
 module.exports = router;
