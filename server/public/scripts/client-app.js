1c0ead3d3025105ce919f5904b01f0e03988c039
Liz Puhl <eapuhl@gmail.com>
2016-08-19 12:23:33 -0500

    Book list clears before genre selector appends

--- server/public/scripts/client-app.js
+++ server/public/scripts/client-app.js
@@ -1,37 +1,99 @@
 $(document).ready(function () {
   getBooks();
 
-  // add a book
+  //select by genre
+  $('#genre-submit').on('click', getByGenre);
+    // add a book
   $('#book-submit').on('click', postBook);
+  $('#book-list').on('click', '.update', putBook);
+  $('#book-list').on('click', '.delete', deleteBook);
 });
 /**
  * Retrieve books from server and append to DOM
  */
-function getBooks() {
-  $.ajax({
-    type: 'GET',
-    url: '/books',
-    success: function (books) {
-      console.log('GET /books returns:', books);
-      books.forEach(function (book) {
-        var pubDate = new Date(book.published);
-        var $el = $('<li></li>');
-        $el.append('<strong>' + book.title + '</strong>');
-        $el.append(' <em>' + book.author + '</em>');
-        $el.append('<div class="details">');
-          $el.append(' <time>' + pubDate.toLocaleDateString() + ': </time>');
-          $el.append(book.edition +' Edition, ');
-          $el.append(' <span id="publisher">' + book.publisher + '</span>');
-          $el.append('</div>');
-        $('#book-list').append($el);
-      });
-    },
 
-    error: function (response) {
-      console.log('GET /books fail. No books could be retrieved!');
-    },
-  });
-}
+ function getByGenre() {
+   $('#book-list').empty();
+   var genreSelected = $('#genre-select').val();
+   console.log (genreSelected);
+  $.ajax({
+    type: 'GET',
+    url: '/books/' + genreSelected,
+    success: function (books) {
+      console.log('GET /books returns:', books);
+      books.forEach(function (book) {
+        var $el = $('<div></div>');
+        var bookProperties = ['title','author','published','genre'];
+
+        bookProperties.forEach(function(property){
+          var inputType = 'text';
+          if (property == 'published') {
+             book[property] = new Date(book[property]);
+            //get strings for month/day/year
+             var month = book[property].getUTCMonth(book[property]) + 1; //months from 1-12
+             var day = book[property].getUTCDate(book[property]);
+             var year = book[property].getUTCFullYear(book[property]);
+           //concatenate into one string month/day/year and set to book.published as text
+           book[property] = month + "/" + day + "/" + year;
+           }   
+          var $input = $('<input type ="text" id="'+property+'"name="'+property +'"/>')
+          $input.val(book[property]);
+          $el.append($input);
+        });
+        $el.data('bookId', book.id);
+        //we use this to uniquely identify it by the book.id
+        $el.append('<button class ="update">Update</button>');
+        $el.append('<button class ="delete">Delete</button>');
+        $('#book-list').append($el);
+      });
+    },
+
+    error: function (response) {
+      console.log('GET /books fail. No books could be retrieved!');
+    },
+  });
+ }
+
+
+ function getBooks() {
+  $.ajax({
+    type: 'GET',
+    url: '/books',
+    success: function (books) {
+
+      console.log('GET /books returns:', books);
+      books.forEach(function (book) {
+        var $el = $('<div></div>');
+        var bookProperties = ['title','author','published','genre'];
+
+        bookProperties.forEach(function(property){
+          var inputType = 'text';
+          if (property == 'published') {
+             book[property] = new Date(book[property]);
+            //get strings for month/day/year
+             var month = book[property].getUTCMonth(book[property]) + 1; //months from 1-12
+             var day = book[property].getUTCDate(book[property]);
+             var year = book[property].getUTCFullYear(book[property]);
+           //concatenate into one string month/day/year and set to book.published as text
+           book[property] = month + "/" + day + "/" + year;
+           }   
+          var $input = $('<input type ="text" id="'+property+'"name="'+property +'"/>')
+          $input.val(book[property]);
+          $el.append($input);
+        });
+        $el.data('bookId',book.id);
+        //we use this to uniquely identify it by the book.id
+        $el.append('<button class ="update">Update</button>');
+        $el.append('<button class ="delete">Delete</button>');
+        $('#book-list').append($el);
+      });
+    },
+
+    error: function (response) {
+      console.log('GET /books fail. No books could be retrieved!');
+    },
+  });
+ }
 /**
  * Add a new book to the database and refresh the DOM
  */
@@ -57,5 +119,43 @@ function postBook() {
     error: function (response) {
       console.log('POST /books does not work...');
     },
+  });
+}
+
+function putBook() {
+  var book = {};
+  var inputs = $(this).parent().children().serializeArray();
+  $.each(inputs, function (i, field) {
+    book[field.name] = field.value;
+  });
+  console.log('books we are putting', book);
+  var bookId = $(this).parent().data('bookId');
+  $.ajax({
+    type: 'PUT',
+    url: '/books/' + bookId,
+    data: book,
+    success: function () {
+      $('#book-list').empty();
+      getBooks();
+    },
+    error: function () {
+      console.log('Error PUT /books/' + bookId);
+    },
+  });
+};
+
+function deleteBook() {
+  var bookId = $(this).parent().data('bookId');
+  $.ajax({
+    type: 'DELETE',
+    url: '/books/' + bookId,
+    success: function () {
+      console.log('DELETE success');
+      $('#book-list').empty();
+      getBooks();
+    },
+    error: function () {
+      console.log('DELETE failed');
+    }
   });
 }
